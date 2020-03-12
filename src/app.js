3f299dbaf77441cbf3a7b7ab86772ba10b653ccc
Likhithanjali-2 <liki2444@gmail.com>
2020-03-12 15:35:47 -0700

    project 2nd part(js file) first commit

--- src/app.js
+++ src/app.js
@@ -26,7 +26,14 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for (let i = 0; i < players.length; i++) {
+        detailedPlayers.push({
+            'name': players[i],
+            'strength':100,
+            'image':"./images/super-" + (i + 1) + ".png",
+            'type':"hero|villain"
+        });
+    }    
     return detailedPlayers;
 }
 
@@ -34,16 +41,42 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random() * 100 + 1);
 }
 
 const buildPlayers = (players, type) => {
     let fragment = '';
+    fragment = '<divclass="player"><imgsrc="./images/super-1.png"alt=""><divclass="name">Hero</div><divclass="strength">100</div></div>';
 
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    // var title = "Building Players";
+    // var container = $("<div>");
+    // container.addClass("player");
+    // var img = $("<img>");
+    // img.src("${players[i].image}");
 
-    return fragment;
+    // var div = $("<div>");
+    // div.addClass("name");
+    // var h4 = $("<h4>");
+    // h4.text("${players[i].name}");
+    // div.append(h4);
+    // container.append(div);
+
+    // div = $("<div>");
+    // div.addClass("name");
+    // h4 = $("<h4>");
+    // h4.text("${players[i].strength}");
+    // div.append(h4);
+
+    // container.append(div);
+    // container.append(img);
+
+    // $("body").append(container);
+    
+    //return container;
+    return fragment
 }
 // Display players in HTML
 const viewPlayers = (players) => {
